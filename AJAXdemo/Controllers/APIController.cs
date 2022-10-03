f24306e41a3a357de58a109c8e69375870482fa2
苡錚 陳 <yikishow@gmail.com>
2022-10-03 19:31:56 +0800

    加入專案檔案。

---
+++ AJAXdemo/Controllers/APIController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AJAXdemo.Controllers
+{
+    public class APIController : Controller
+    {
+        //http://localhost.API/Index
+        public IActionResult Index()
+        {
+            //回應單純的字串 "Hello Ajax"
+            //return Content("<h2>Hello Ajax</h2>"); //會顯示<h2>Hello Ajax</h2> 顯示為文字
+            return Content("<h2>Hello Ajax!!</h2>","text/html",System.Text.Encoding.UTF8); //參數(內容,字串格式,Encoding的類型)
+        }
+
+        
+    }
+}
